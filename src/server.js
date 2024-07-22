bf6ad3a4fc02497b2dc7bfdafdf6dc4c894dbb46
Ryan Starkey <rstarkey.vencas@gmail.com>
2024-07-22 15:33:02 -0700

    changed https to http

--- src/server.js
+++ src/server.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/devices', (req, res) => {
   const devices = [
@@ -16,12 +12,6 @@ app.get('/devices', (req, res) => {
     { id: 3, name: 'Device 3', type: 'alarm', data: [{ value: 'active' }, { value: 'inactive' }] }
   ];
   res.json(devices);
-});
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(port, () => {
